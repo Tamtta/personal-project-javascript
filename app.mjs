314671641f612bd0361b7f17264028218ea6c4f8
TAMTA JOJUA <jojua.tamta99@gmail.com>
2022-06-27 18:33:40 +0400

    added module lms

--- app.mjs
+++ app.mjs
@@ -1,4 +1,5 @@
 import { Subject } from "./subject.mjs";
+import { LMS } from "./lms.mjs";
 
 function main() {
   // Subject
@@ -13,6 +14,14 @@ function main() {
   });
 
   console.log(history.id, math.id);
+
+  // LMS
+  const lms = new LMS();
+  lms.add(history);
+  lms.add(math);
+  // lms.remove(history);
+  console.log(lms.verify(math), lms.verify(history));
+  console.log(lms.readAll());
 }
 
 main();
---
+++ lms.mjs
@@ -0,0 +1,19 @@
+export class LMS {
+  subjects = [];
+
+  add(subject) {
+    this.subjects.push(subject);
+  }
+
+  remove(subject) {
+    this.subjects = this.subjects.filter((s) => s.id != subject.id);
+  }
+
+  verify(subject) {
+    return this.subjects.some((s) => s.id == subject.id);
+  }
+
+  readAll() {
+    return this.subjects;
+  }
+}
